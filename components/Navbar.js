df444eaa169c89563c93cd83d222f3154a4f3af7
chelo603 <chelo_603@hotmail.com>
2022-07-25 19:00:09 -0300

    Desafio2 Aguirre Marcelo

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header className="navbar" >
+ <button> home </button>
+ <p> nosotros </p>
+ <a src='#'> articulos </a>
+ <a src='#'> adopciones </a>
+ <a src='#'> consejos </a>
+  <a src='#'> contactos </a>  
+         </header>
+  );
+}
+
+export default Navbar;
